df21225c24bfc929b55fffe62994b9ebdb26fef2
Julia Belyakova <julbinb@gmail.com>
2021-01-02 15:17:05 -0500

    make analysis Julia 1.0 compatible | v2

--- src/utils/multiset.jl
+++ src/utils/multiset.jl
@@ -3,7 +3,7 @@
 #######################################################################
 
 # For Julia < 1.5 compatibility
-const DICT_MERGE! = isdefined(Base, :mergewith!) ? mergewith! : merge
+const DICT_MERGE! = isdefined(Base, :mergewith!) ? mergewith! : merge!
 
 unionMergeWith!(ms :: Multiset, others :: Multiset...) = begin
     DICT_MERGE!(+, ms.data, map(ms -> ms.data, others)...)
